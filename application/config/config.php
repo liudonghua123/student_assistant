c7507ed4a67eeb5decdbde895440342b0563ec91
liudonghua123 <liudonghua123@gmail.com>
2014-10-25 16:45:16 +0800

    update readme and project root problem

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url'] = 'http://localhost/a3m/';
+$config['base_url'] = 'http://localhost/student_assistant/';
 
 /*
 |--------------------------------------------------------------------------
